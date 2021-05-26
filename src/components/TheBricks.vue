1e4a59add5826e4b334286abfde0a59a5b9aa611
Natalia <ngabrieleva@babson.edu>
2021-05-26 11:27:45 -0400

    Added axios.

--- src/components/TheBricks.vue
+++ src/components/TheBricks.vue
@@ -6,7 +6,7 @@
   </div>
 </form-->
   <brick-filter v-model:inscription="filter.inscription" v-model:reset="resetForm" />
-<div class="bricks container mx-auto max-w-6xl grid grid-cols-1 md:grid-cols-3 gap-4">
+<div class="bricks container mx-auto max-w-6xl grid grid-cols-1 md:grid-cols-4 gap-5">
   <brick-card v-for="brick in filterBricksByName" :key="brick.id" :brick="brick" />
 </div>
   <h3 class="container mx-auto max-w-6xl msg_no_results text-3xl" v-if="filterBricksByName.length === 0">No bricks match your criteria</h3>
@@ -15,6 +15,7 @@
 </template>
 
 <script>
+import axios from "axios";
 import BrickCard from './BrickCard.vue';
 import BrickFilter from './BrickFilter.vue';
 
@@ -76,7 +77,45 @@ export default {
   methods: {
     resetForm() {
       this.filter.inscription = '';
+    },
+    async fetchBricks() {
+      try {
+        const url = `http://bell.babson.edu/jsonapi/bricks?fields[file--file]=uri,url`
+        axios.defaults.withCredentials = true;
+        const response = await axios.get(url, {}, {
+              headers: {
+      'crossDomain': true,
+      'Content-Type': 'application/json'
     }
+  // auth: {
+  //   username: babson,
+  //   password: drupal9
+  // }
+  })
+  console.log(response);
+        const results = response.data.data
+        this.bricks = results.map(bricks => ({
+          id: bricks.attributes.milleniumID,
+          number: bricks.attributes.brickNumber,
+          inscription: bricks.attributes.brickInscription,
+          imgLink: 'src/assets/bricks/IMG_2008.jpg',
+          zone: 'zone1'
+        }))
+      } catch (err) {
+        if (err.response) {
+          // client received an error response (5xx, 4xx)
+          console.log("Server Error:", err)
+        } else if (err.request) {
+          // client never received a response, or request never left
+          console.log("Network Error:", err)
+        } else {
+          console.log("Client Error:", err)
+        }
+      }
+    },
+  },
+  mounted() {
+    this.fetchBricks();
   }
 }
 </script>
